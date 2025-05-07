56e30cc26facf9e1dcef48e120ba28e470101cc3
richard_amorim <richard_amorim@estudante.sesisenai.org.br>
2025-05-07 19:42:03 -0300

    First Commit

--- src/main/java/com/senai/gerenciamento_hotel/controllers/ClienteController.java
+++ src/main/java/com/senai/gerenciamento_hotel/controllers/ClienteController.java
@@ -1,4 +1,49 @@
 package com.senai.gerenciamento_hotel.controllers;
 
+import com.senai.gerenciamento_hotel.dtos.ClienteDTO;
+import com.senai.gerenciamento_hotel.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cliente")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ClienteDTO> createCliente(@RequestBody ClienteDTO clienteDTO) {
+        ClienteDTO novoCliente = clienteService.createCliente(clienteDTO);
+        return new ResponseEntity<>(novoCliente, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<ClienteDTO> getClienteById(@PathVariable Long id) {
+        ClienteDTO clienteDTO = clienteService.getClienteById(id);
+        return ResponseEntity.ok(clienteDTO);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<ClienteDTO>> getAllClientes() {
+        List<ClienteDTO> list = clienteService.findByAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ClienteDTO> updateCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteDTO) {
+        ClienteDTO cliente = clienteService.updateCliente(id, clienteDTO);
+        return ResponseEntity.ok(cliente);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
+        clienteService.deleteCliente(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/senai/gerenciamento_hotel/services/ClienteService.java
+++ src/main/java/com/senai/gerenciamento_hotel/services/ClienteService.java
@@ -1,4 +1,20 @@
 package com.senai.gerenciamento_hotel.services;
 
+
+import com.senai.gerenciamento_hotel.dtos.ClienteDTO;
+import com.senai.gerenciamento_hotel.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public ClienteDTO createCliente(ClienteDTO clienteDTO) {
+
+    }
+
+
+    public ClienteDTO getClienteById(Long id) {
+    }
 }
