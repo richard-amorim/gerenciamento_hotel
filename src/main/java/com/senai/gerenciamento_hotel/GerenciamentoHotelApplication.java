b66940d2b31094656696b2eef9ea691f31117371
richard_amorim <richard_amorim@estudante.sesisenai.org.br>
2025-05-07 19:04:02 -0300

    first commit

---
+++ src/main/java/com/senai/gerenciamento_hotel/GerenciamentoHotelApplication.java
@@ -0,0 +1,13 @@
+package com.senai.gerenciamento_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoHotelApplication.class, args);
+    }
+
+}
